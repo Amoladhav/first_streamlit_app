3b8973ee430d3bfcd102c8a8e106ef11eb5a29c1
Amol Adhav <amol.adhav@gmail.com>
2022-08-28 13:05:02 -0700

    stop snowflake

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,6 +34,9 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_c
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # Writes into a table
 streamlit.dataframe(fruityvice_normalized)
+# don't run anythying
+streamlit.stop()
+
 import snowflake.connector
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
