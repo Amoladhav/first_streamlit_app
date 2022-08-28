937b6b75bd30e83d1a4d54d66c8eef71e26b900f
Amol Adhav <amol.adhav@gmail.com>
2022-08-28 13:30:54 -0700

    add snowflake fetch function

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,6 +19,12 @@ def get_fruityvice_data(this_fruit_choice):
         fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
         return fruityvice_normalized
 
+def get_fruit_load_list():
+    With my_cnx.cursor() as my_cur:
+        my_cur.execute("SELECT * FROM fruit_load_list")
+        return my_cur.fetchall()
+
+
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt",)
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
@@ -44,18 +50,12 @@ streamlit.write('The user entered ', fruit_choice)
 
 
 # don't run anythying
-streamlit.stop()
+if streamlit.button('Get Fruit Load List'):
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    my_data_rows = get_fruit_load_list()
+    streamlit.dataframe(my_data_rows)
 
+# fruit2_choice = streamlit.text_input("What fruit would you like to add?", 'Kiwi')
+# streamlit.write('Thanks for adding ', fruit2_choice)
 
-
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT * FROM fruit_load_list")
-my_data_rows = my_cur.fetchall()
-streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_rows)
-
-fruit2_choice = streamlit.text_input("What fruit would you like to add?", 'Kiwi')
-streamlit.write('Thanks for adding ', fruit2_choice)
-
-my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values ('from Streamlit');")
+# my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values ('from Streamlit');")
